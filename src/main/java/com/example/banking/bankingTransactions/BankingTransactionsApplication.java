dbeb5a875b425fe7974cf304133345e87dc0bdd4
Jonathan David Yaguachi Pereira <jyaguach@bancolombia.com.co>
2024-09-15 15:23:09 -0500

    Initial commit

---
+++ src/main/java/com/example/banking/bankingTransactions/BankingTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.banking.bankingTransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/banking/bankingTransactions/BankingTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banking.bankingTransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
